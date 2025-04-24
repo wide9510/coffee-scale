f293ddd356b103b3e35e336cc5e276151714bdcf
wide9510 <49491361+wide9510@users.noreply.github.com>
2025-04-24 16:40:21 -0600

    Initial commit

---
+++ doc/conf.py
@@ -0,0 +1,25 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'NCS Example Application'
+copyright = '2024, The Zephyr Community'
+author = 'The Zephyr Community'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build_sphinx', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
---
+++ doc/index.rst
@@ -0,0 +1,10 @@
+Welcome to NCS Example Application's documentation!
+===================================================
+
+This is a simple Sphinx documentation setup for ``ncs-example-application``.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
